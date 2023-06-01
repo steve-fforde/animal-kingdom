543eba2ee043c4d1f8ffc1112f2be459ce4aea28
Steve Cotton <steve.m.cotton@gmail.com>
2023-06-01 11:17:14 +0100

    first commit

---
+++ java_project/src/main/java/starters/Library.java
@@ -0,0 +1,7 @@
+package starters;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ java_project/src/test/java/starters/LibraryTest.java
@@ -0,0 +1,12 @@
+package starters;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+
+    @Test void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertEquals(true, classUnderTest.someLibraryMethod());
+    }
+}
